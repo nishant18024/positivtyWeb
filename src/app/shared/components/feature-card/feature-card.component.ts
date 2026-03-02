5aad95a4a12f707c4d63891721edf96560a9fd20
Shubham3803 <Work.shubham1601@gmail.com>
2026-03-02 18:42:18 +0530

    Resolved layout, responsiveness, and alignment issues in the header component.

--- src/app/shared/components/feature-card/feature-card.component.ts
+++ src/app/shared/components/feature-card/feature-card.component.ts
@@ -3,12 +3,14 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-feature-card',
+  standalone: true, // ✅ ADD THIS
   imports: [CommonModule],
   templateUrl: './feature-card.component.html',
-  styleUrl: './feature-card.component.scss'
+  styleUrls: ['./feature-card.component.scss'] // ✅ small correction (plural)
 })
 export class FeatureCardComponent {
   @Input() image!: string;
-  @Input() title!: string;
-  @Input() description!: string;
+ @Input() title: string = '';
+  @Input() subtitle: string = '';
+  @Input() description: string = '';
 }
