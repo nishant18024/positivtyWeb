e8aa5b120d47b85ea1920ecdda13e019b33dab79
Shubham3803 <Work.shubham1601@gmail.com>
2026-03-07 20:54:24 +0530

    fixing talent hero section with responsive

--- src/app/pages/enterprises/enterprises.component.ts
+++ src/app/pages/enterprises/enterprises.component.ts
@@ -1,18 +1,25 @@
 import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { RouterLink } from '@angular/router';
 import { Component, Inject, OnDestroy, OnInit, PLATFORM_ID } from '@angular/core';
 import { EnterprisesInfoCardComponent, InfoCard } from "../../shared/components/enterprises-info-card/enterprises-info-card.component";
 import { EnterprisesCareCardComponent } from "../../shared/components/enterprises-care-card/enterprises-care-card.component";
 import { EnterprisesStepsCardComponent } from "../../shared/components/enterprises-steps-card/enterprises-steps-card.component";
 import { EmployeesStepsCardComponent } from "../../shared/components/employees-steps-card/employees-steps-card.component";
+import { FaqComponent } from "../../shared/components/faqs/faqs.component";
 
 interface ImpactItem {
   percentage: string;
   text: string;
 }
+interface FaqItem {
+  q: string;
+  a: string[];
+}
 
 @Component({
   selector: 'app-enterprises',
-  imports: [CommonModule, EnterprisesInfoCardComponent, EnterprisesCareCardComponent, EnterprisesStepsCardComponent, EmployeesStepsCardComponent],
+  standalone: true,
+  imports: [CommonModule, RouterLink, EnterprisesInfoCardComponent, EnterprisesCareCardComponent, EnterprisesStepsCardComponent, EmployeesStepsCardComponent, FaqComponent],
   templateUrl: './enterprises.component.html',
   styleUrl: './enterprises.component.scss'
 })
@@ -44,7 +51,7 @@ export class EnterprisesComponent implements OnInit, OnDestroy {
   }
 
 
-  //  info card list 
+  //  info card list
   cards: InfoCard[] = [
     {
       id: 1,
@@ -218,6 +225,44 @@ export class EnterprisesComponent implements OnInit, OnDestroy {
       text: 'felt improved emotional wellbeing'
     }
   ];
+
+  // ==============>
+
+   faqData: FaqItem[] = [
+      {
+        q: 'How Does a School/University Mental Health Wellness Program Work?',
+        a: [
+          'The Enterprise Mental Health Wellness Program by Positivty is a comprehensive solution for ensuring the mental well-being of your workforce. For Enterprises: The process begins when an enterprise fills out a booking form, providing their organizational and contact person’s details and selecting a mental health package tailored to their employees needs. Once this is done, the enterprise can proceed to customize the coupons for their workforce by selecting different counts and amounts. An additional feature allows enterprises to upload an email file containing employee email IDs, enabling Positivty to deliver e-coupons directly to employees on behalf of the organization. After customization, the details are reviewed and confirmed, followed by payment. Once the transaction is complete, personalized e-coupons are sent to the listed employees along with notifications for the employer. For Employees: Employees receiving the e-coupons can create a profile or log in to the platform. They are then able to explore a network of certified and experienced therapists, selecting one that meets their specific preferences or needs. After choosing the therapist and relevant services, employees can schedule therapy sessions by picking time slots that best suit their availability, including weekends and public holidays. At the time of payment, the e-coupon code provided by their employer can be redeemed, with any additional costs covered through a simple top-up. Employees are then all set to attend their scheduled sessions, beginning their journey towards improved mental health and well-being with notifications keeping them informed throughout.',
+        ]
+      },
+      {
+        q: 'What does the Enterprise Mental Wellness Program include? ',
+        a: [
+          'The Enterprise Mental Wellness Program offers an all-encompassing solution to address the unique needs of organizations and their employees. It includes confidential one-on-one therapy sessions with certified therapists, comprehensive workforce mental health assessments, and regular insights into overall well-being. Additionally, ongoing support is provided through personalized wellness plans, which include access to self-care tools and resources to manage various mental health challenges. The program also ensures cultural adaptability by offering support in regional languages to cater to diverse employee needs, creating a holistic and inclusive approach to mental health care.'
+        ]
+      },
+      {
+        q: 'How much does the package cost? ',
+        a: ['The cost of the mental wellness package depends on several factors, including the size of your workforce and the specific services or packages chosen. Since each organization has unique requirements, Positivty provides customized quotes based on the selected options. Contact us directly to discuss your needs, and we’ll help design a flexible pricing plan that aligns with your budget while maximizing the value for your employees.'
+        ]
+      },
+      {
+        q: 'Is therapy confidential for employees?  ',
+        a: [`Confidentiality is a cornerstone of Positivty’s services. All personal details, session content, and mental health records of employees are kept strictly private and are protected with state-of-the-art encryption and privacy protocols. No information is shared with employers or third parties without the explicit consent of the individual. This ensures that employees feel safe and supported in seeking the help you need.`
+        ]
+      },
+      {
+        q: 'Can employees choose their therapist?   ',
+        a: [`Yes, employees can browse therapist profiles and select one based on their preferences and requirements. Positivty empowers employees to take control of their mental health journey by providing the freedom to select their therapist. Employees can explore detailed profiles of certified therapists, reviewing their qualifications, areas of expertise, and experience. This flexibility allows them to choose a professional who aligns with their personal preferences, such as language, gender, or specialization, ensuring you receive the best possible care tailored to their needs`
+        ]
+      },
+      {
+        q: 'How do mental health wellness packages benefit employees in the workplace?  ',
+        a: [`Mental health wellness packages offer numerous benefits to employees by providing access to professional support, resources for managing stress, and personalized care plans. These services help employees navigate challenges such as work-related pressure, personal issues, and emotional fatigue, fostering a sense of well-being. Positivty’s initiative promotes healthier, more engaged, and motivated teams by addressing mental health concerns proactively. The confidentiality of the program ensures that employees feel secure while seeking help, contributing to a supportive and productive workplace environment.`
+        ]
+      }
+
+    ];
 
   currentIndex = 0;
   intervalId: any;
